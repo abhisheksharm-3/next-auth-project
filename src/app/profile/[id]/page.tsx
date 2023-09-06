90c6ec1f61da5f16e68fff9e067ded597530e5bc
Abhishek <abhitiku2003@gmail.com>
2023-09-06 16:10:21 +0530

    added coming soon

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,12 +1,19 @@
+import React from "react";
+import "animate.css";
+
 export default function userProfile({ params }: any) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Profile</h1>
-      <hr />
-      <p className="text-4xl">
-        Profile Page{" "}
-        <span className="px-2 bg-red-600 rounded">{params.id}</span>
-      </p>
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold animate__animated animate__bounce infinite">
+          Coming Soon
+        </h1>
+        <hr className="w-16 h-1 bg-white mx-auto my-4" />
+        <p className="text-lg animate__animated animate__fadeIn">
+          Profile Page{" "}
+          <span className="px-2 bg-red-600 rounded">{params.id}</span>
+        </p>
+      </div>
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,18 @@
+import React from "react";
+import "animate.css";
+
 export default function ProfilePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Profile</h1>
-      <hr />
-      <p>Profile Page</p>
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold animate__animated animate__bounce infinite">
+          Coming Soon
+        </h1>
+        <hr className="w-16 h-1 bg-white mx-auto my-4" />
+        <p className="text-lg animate__animated animate__fadeIn">
+          Stay tuned for updates!
+        </p>
+      </div>
     </div>
   );
 }
